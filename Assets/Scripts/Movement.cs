4aff447844f0c96491874734d23b36efcb72cd55
sercan1123 <sercan_saglam19@hotmail.com>
2022-03-09 20:00:27 +0300

    commit

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,6 +10,8 @@ public class Movement : MonoBehaviour
     float speed =5f;
     [SerializeField]
     float jumpPower = 13f;
+    [SerializeField]
+    float turnspeed = 15;
     void Start()
     {
         rb = GetComponent<Rigidbody>(); // Yeni bir rigidbody oluşturduk new leme işlemi
@@ -27,20 +29,21 @@ public class Movement : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = new Vector3(rb.velocity.x,Mathf.Clamp((jumpPower * 100) * Time.deltaTime,0f,15f),0f);    
-            Debug.Log("Jump");
         }
         else if(Input.GetKey(KeyCode.A))
         {
             // transform.Rotate(0f,-180f,0f); Her A ya basılı tuttuğumuzda y eksenine -180 ekleyecek. bu yüzden rotation kullandık eşitleme ile.
-            transform.rotation = Quaternion.Euler(0f,-180f,0f);
+            // transform.rotation = Quaternion.Euler(0f,-180f,0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f,-180f,0f),turnspeed * Time.deltaTime);
             rb.velocity = new Vector3(Mathf.Clamp((-speed *100) * Time.deltaTime,-15f,0f),rb.velocity.y,0f);
-            Debug.Log("Left");
         }
         else if(Input.GetKey(KeyCode.D))
         {
-            transform.rotation = Quaternion.Euler(0f,0f,0f);
+            // transform.rotation = Quaternion.Euler(0f,0f,0f);
+            // Yumuşak bir dönüş için Quaternion.Lerp komutu kullanılır.
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f,0f,0f),turnspeed * Time.deltaTime);
             rb.velocity = new Vector3(Mathf.Clamp((speed *100) * Time.deltaTime,0f,15f),rb.velocity.y,0f);
-            Debug.Log("Right");
+
         }
         else
         {
