d195a664adedfe3d3bcf535b07b7044adde0446a
sercan1123 <sercan_saglam19@hotmail.com>
2022-03-21 02:20:36 +0300

    commit

--- Assets/Scripts/ChooseWeapons.cs
+++ Assets/Scripts/ChooseWeapons.cs
@@ -9,6 +9,17 @@ public class ChooseWeapons : MonoBehaviour
    [SerializeField] private Transform FirePos;
    [SerializeField] private int ammoCount;
 
+    public int GetCurrentWeaponAmmoCount
+    {
+        get
+        {
+            return ammoCount;
+        }
+        set
+        {
+            ammoCount = value;
+        }
+    }
     void Update()
     {
         SelectWeapons();
@@ -23,16 +34,24 @@ public class ChooseWeapons : MonoBehaviour
         switch(Input.inputString)
         {
             case "1":
+                // Silahların kalan mermi sayılarını tutmamız için gerekli kod.
+
+                // GetCurrentWeaponAmmoCount = attack.weapons[1].gameObject.GetComponent<Attack>().GetAmmo;
+                // GetCurrentWeaponAmmoCount = attack.weapons[2].gameObject.GetComponent<Attack>().GetAmmo;
                 attack.weapons[0].SetActive(true);
                 attack.weapons[1].SetActive(false);
                 attack.weapons[2].SetActive(false);
                 break;
             case "2":
+                //  GetCurrentWeaponAmmoCount = attack.weapons[0].GetComponent<Attack>().GetAmmo;
+                //  GetCurrentWeaponAmmoCount = attack.weapons[2].GetComponent<Attack>().GetAmmo;
                  attack.weapons[1].SetActive(true);
                  attack.weapons[0].SetActive(false);
                  attack.weapons[2].SetActive(false);
                 break;
             case "3":
+                 ammoCount = attack.weapons[0].GetComponent<Attack>().GetAmmo;
+                 ammoCount = attack.weapons[1].GetComponent<Attack>().GetAmmo;
                  attack.weapons[2].SetActive(true);
                  attack.weapons[0].SetActive(false);
                  attack.weapons[1].SetActive(false);
